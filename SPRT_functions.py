f2417afa9fa34d315ff3a228c21c9488680275c0
peterpirogtf <peterpirogtf@gmail.com>
2020-03-24 15:11:24 +0100

    New files added

--- SPRT_functions.py
+++ SPRT_functions.py
@@ -1,12 +1,13 @@
 #GTC ==1.2.0
 from GTC import *
+import numpy as np
 
 class PRT():
     def __init__(self,model=None,numer=None):
         self.model=model
         self.numer=numer
         self.R0=None
-        self.Wt=np.arr
+        self.Wt=np.array([])
 
     def metoda1(self):
         print("model to", self.model)
