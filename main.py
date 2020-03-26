e7ee8954decbb89c48b6c6913569e13194fe6f27
peterpirogtf <peterpirogtf@gmail.com>
2020-03-26 14:46:00 +0100

    New files added

--- main.py
+++ main.py
@@ -3,16 +3,13 @@ import GTC
 import numpy as np
 
 
-#term1=SPRT.PRT(model='5685',numer='1114')
+term1=SPRT.PRT(model='5685',numer='1114')
 
-
-#term1.metoda1()
-#term2.metoda1()
 
 #term1.R0.x=102
 
-#           T=GTC.ureal(29.764663066392302,0.005,label='temp')
-#           W=GTC.ureal(1.11813889,1.982113655829038e-05,label='W')
+T=GTC.ureal(29.764663066392302,0.005,label='temp')
+W=GTC.ureal(1.11813889,1.982113655829038e-05,label='W')
 
 #print(SPRT.Conv_kelvin2celsius(T))
 #SPRT.Calculate_Wr(T,verbose=True)
@@ -27,7 +24,8 @@ unc_array=SPRT.lists2unc_array(value_list,unc_list)
 
 print('value_list=',value_list)
 print('unc_list=',unc_list)
-print(unc_array)
-
+print(unc_array) # tutaj jest wytworzona macierz liczb z niepewnoscią
+print(GTC.sin(unc_array)) #tutaj można zrobic ich sinus
 
 #print(SPRT.Calculate_temp_from_W(1.0794875093598515))
+
