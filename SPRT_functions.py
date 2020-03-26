3cdf0e620d46d20495a88fdfe1dd34ecc00becf0
peterpirogtf <peterpirogtf@gmail.com>
2020-03-26 14:43:29 +0100

    New files added

--- SPRT_functions.py
+++ SPRT_functions.py
@@ -1,19 +1,49 @@
-#GTC ==1.2.0
+# GTC ==1.2.0
 import GTC
 import numpy as np
 import pandas as pd
 
-#ITS-90 functionc  calculated  according to "Guide to the realization of the ITS-90", platinum resistance termometry
-#import numpy as np
+# ITS-90 functionc  calculated  according to "Guide to the realization of the ITS-90", platinum resistance termometry
+# import numpy as np
 import GTC
 
-def lists2unc_array(value_list,unc_list=None,k=2):
-    nrow=len(value_list)
 
-    print('nrow=',nrow)
+def lists2unc_array(value_list, unc_list=0.0, k=2, value_name='X'):
+    # INPUT variables:
+    # for value_list are:
+    #       <class 'list'>
+    #       <class 'numpy.ndarray'>
+    # for unc_list are:
+    #       <class 'list'>
+    #       <class 'numpy.ndarray'>
+    #       <class 'int'>
+    #       <class 'float'>
+    # for k are:
+    #       <class 'int'>
+    #       <class 'float'>
+    # for value_name are:
+    #       <class 'string'>
 
-    return value_list
 
+
+    # find size of value_list
+    n_rows = len(value_list)
+    n_cols = len(value_list[0])
+
+    realnumber_list = [[0 for x in range(n_cols)] for x in range(n_rows)]
+
+    for i in range(n_rows):
+        for j in range(n_cols):
+            val_label = value_name + str(i) + '_' + str(j)
+            if isinstance(unc_list, int) or isinstance(unc_list, float):
+                unc_std = unc_list / k
+            else:
+                unc_std = unc_list[i][j] / k
+            realnumber_list[i][j] = GTC.ureal(x=value_list[i][j], u=unc_std, label=val_label)
+
+    unc_array = GTC.la.uarray(realnumber_list)
+
+    return unc_array
 
 
 def isGTC(value):
@@ -22,6 +52,7 @@ def isGTC(value):
         return True
     else:
         return False
+
 
 # converting float type to GTC uncertainty type
 def float2GTC(value):
@@ -32,130 +63,132 @@ def float2GTC(value):
 
 
 def Conv_celsius2kelvin(temp_c):
-    return float2GTC(temp_c)+273.15
+    return float2GTC(temp_c) + 273.15
+
 
 def Conv_kelvin2celsius(temp_k):
-    return float2GTC(temp_k)-273.15
+    return float2GTC(temp_k) - 273.15
 
-def Calculate_Wr(temp_C,verbose=False):
+
+def Calculate_Wr(temp_C, verbose=False):
     # constants for temperature range -259.3467 C to 0 C
-    A=[-2.13534729, #A0
-       3.18324720, #A1
-       -1.80143597,#A2
-       0.71727204, #A3
-       0.50344027,#A4
-       -0.61899395,#A5
-       -0.05332322, #A6
-       0.28021362, #A7
-       0.10715224, #A8
-       -0.29302865,#A9
-       0.04459872,#A10
-       0.11868632,#A11
-       -0.05248134]#A12
+    A = [-2.13534729,  # A0
+         3.18324720,  # A1
+         -1.80143597,  # A2
+         0.71727204,  # A3
+         0.50344027,  # A4
+         -0.61899395,  # A5
+         -0.05332322,  # A6
+         0.28021362,  # A7
+         0.10715224,  # A8
+         -0.29302865,  # A9
+         0.04459872,  # A10
+         0.11868632,  # A11
+         -0.05248134]  # A12
     # constants for temperature range 0 C to 961.78 C
-    C = [2.78157254,#C0
-         1.64650916,#C1
-         -0.13714390,#C2
-         -0.00649767,#C3
-         -0.00234444,#C4
-         0.00511868,#C5
-         0.00187982,#C6
-         -0.00204472,#C7
-         -0.00046122,#C8
-         0.00045724]#C9
-    temp_C=float2GTC(temp_C)
-    temp_K=Conv_celsius2kelvin(temp_C)
-    Wr=1
-    if (temp_C.x>= -259.3467) and (temp_C.x<0):
+    C = [2.78157254,  # C0
+         1.64650916,  # C1
+         -0.13714390,  # C2
+         -0.00649767,  # C3
+         -0.00234444,  # C4
+         0.00511868,  # C5
+         0.00187982,  # C6
+         -0.00204472,  # C7
+         -0.00046122,  # C8
+         0.00045724]  # C9
+    temp_C = float2GTC(temp_C)
+    temp_K = Conv_celsius2kelvin(temp_C)
+    Wr = 1
+    if (temp_C.x >= -259.3467) and (temp_C.x < 0):
         for i in range(13):
-            if i==0:
-                sum=A[0]
+            if i == 0:
+                sum = A[0]
             else:
-                sum=sum+A[i]*GTC.pow((GTC.log(temp_K/273.16)+1.5)/1.5,i)
-        Wr=GTC.exp(sum)
-    elif (temp_C.x>= 0) and (temp_C.x<=961.78):
+                sum = sum + A[i] * GTC.pow((GTC.log(temp_K / 273.16) + 1.5) / 1.5, i)
+        Wr = GTC.exp(sum)
+    elif (temp_C.x >= 0) and (temp_C.x <= 961.78):
         for i in range(10):
-            if i==0:
-                sum=C[0]
+            if i == 0:
+                sum = C[0]
             else:
 
-                sum=sum+C[i]*GTC.pow((temp_K-754.15)/481,i)
-        Wr=sum
+                sum = sum + C[i] * GTC.pow((temp_K - 754.15) / 481, i)
+        Wr = sum
     else:
         print("temperature out of range")
     if verbose:
-        print("Wr({}, unc std:{})={}, unc std:{}".format(temp_C.x,temp_C.u,Wr.x,Wr.u))
+        print("Wr({}, unc std:{})={}, unc std:{}".format(temp_C.x, temp_C.u, Wr.x, Wr.u))
     return Wr
-def Calculate_temp_from_W(W,verbose=False):
-    W=float2GTC(W)
+
+
+def Calculate_temp_from_W(W, verbose=False):
+    W = float2GTC(W)
     # constants for temperature  calculation range -259.3467 C to 0 C
-    B=[0.183324722,#B0
-       0.240975303,#B1
-       0.209108771,#B2
-       0.190439972,#B3
-       0.142648498,#B4
-       0.077993465,#B5
-       0.012475611,#B6
-       -0.032267127,#B7
-       -0.075291522,#B8
-       -0.056470670,#B9
-       0.076201285,#B10
-       0.123893204,#B11
-       -0.029201193,#B12
-       -0.091173542,#B13
-       0.001317696,#B14
-       0.026025526]#B15
+    B = [0.183324722,  # B0
+         0.240975303,  # B1
+         0.209108771,  # B2
+         0.190439972,  # B3
+         0.142648498,  # B4
+         0.077993465,  # B5
+         0.012475611,  # B6
+         -0.032267127,  # B7
+         -0.075291522,  # B8
+         -0.056470670,  # B9
+         0.076201285,  # B10
+         0.123893204,  # B11
+         -0.029201193,  # B12
+         -0.091173542,  # B13
+         0.001317696,  # B14
+         0.026025526]  # B15
     # constants for temperature calculation range 0 C to 961.78 C
-    D=[439.932854, #D0
-       472.418020, #D1
-       37.684494,#D2
-       7.472018,#D3
-       2.920828,#D4
-       0.005184,#D5
-       -0.963864,#D6
-       -0.188732,#D7
-       0.191203,#D8
-       0.049025]#D9
-    temp_k=0
-    if W.x<=1:
+    D = [439.932854,  # D0
+         472.418020,  # D1
+         37.684494,  # D2
+         7.472018,  # D3
+         2.920828,  # D4
+         0.005184,  # D5
+         -0.963864,  # D6
+         -0.188732,  # D7
+         0.191203,  # D8
+         0.049025]  # D9
+    temp_k = 0
+    if W.x <= 1:
         for i in range(16):
-            if i==0:
-                sum=B[0]
+            if i == 0:
+                sum = B[0]
             else:
-                sum=sum+B[i]*GTC.pow((GTC.pow(W,1/6)-0.65)/0.35,i)
-        temp_k=273.16*sum
+                sum = sum + B[i] * GTC.pow((GTC.pow(W, 1 / 6) - 0.65) / 0.35, i)
+        temp_k = 273.16 * sum
     else:
         for i in range(10):
-            if i==0:
-                sum=0
+            if i == 0:
+                sum = 0
             else:
-                sum=sum+D[i]*GTC.pow((W-2.64)/1.64,i)
-        temp_k=273.15+D[0]+sum
+                sum = sum + D[i] * GTC.pow((W - 2.64) / 1.64, i)
+        temp_k = 273.15 + D[0] + sum
     temp_C = Conv_kelvin2celsius(temp_k)
     if verbose:
-        #print("W({})={}".format(temp_c, W))
-        print("T({}, unc std:{})={}, unc std:{}".format(W.x, W.u,temp_C.x, temp_C.u))
+        # print("W({})={}".format(temp_c, W))
+        print("T({}, unc std:{})={}, unc std:{}".format(W.x, W.u, temp_C.x, temp_C.u))
     return temp_C
 
 
-
 class PRT():
-    def __init__(self,model=None,numer=None,nazwa_pliku_xls='Dane_termometrow.xlsx'):
-        self.model=model
-        self.numer=numer
-        self.plik_danych=nazwa_pliku_xls
+    def __init__(self, model=None, numer=None, nazwa_pliku_xls='Dane_termometrow.xlsx'):
+        self.model = model
+        self.numer = numer
+        self.plik_danych = nazwa_pliku_xls
         self.__wczytaj_dane_sondy()
-        #self.Wt=np.array([])
-        #self.R0 = ureal()
+        # self.Wt=np.array([])
+        # self.R0 = ureal()
 
-    def __odczyt2wektor(self,odczyt,niepewnosc_rozszerzona=0):
-         return 1
+    def __odczyt2wektor(self, odczyt, niepewnosc_rozszerzona=0):
+        return 1
 
     def __wczytaj_dane_sondy(self):
-        self.data=pd.read_excel(io=self.plik_danych,sheet_name=self.model+'_'+self.numer)
+        self.data = pd.read_excel(io=self.plik_danych, sheet_name=self.model + '_' + self.numer)
         print(self.data)
 
     def metoda1(self):
         print("model to", self.model)
         print("numer to", self.numer)
-
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import SPRT_functions as SPRT
 import GTC
+import numpy as np
 
 
-term1=SPRT.PRT(model='5685',numer='1114')
+#term1=SPRT.PRT(model='5685',numer='1114')
 
 
 #term1.metoda1()
@@ -10,17 +11,22 @@ term1=SPRT.PRT(model='5685',numer='1114')
 
 #term1.R0.x=102
 
-T=GTC.ureal(29.764663066392302,0.005,label='temp')
-W=GTC.ureal(1.11813889,1.982113655829038e-05,label='W')
+#           T=GTC.ureal(29.764663066392302,0.005,label='temp')
+#           W=GTC.ureal(1.11813889,1.982113655829038e-05,label='W')
 
 #print(SPRT.Conv_kelvin2celsius(T))
 #SPRT.Calculate_Wr(T,verbose=True)
 #SPRT.Calculate_temp_from_W(W,verbose=True)
 
 value_list=[[1,2,3,4],[5,6,7,8]]
-unc_list=[[0.1,0.2,0.3,0.4],[0.01,0.02,0.03,0.04]]
+#unc_list=[[0.1,0.2,0.3,0.4],[0.01,0.02,0.03,0.04]]
 
+unc_list=1
+value_list=np.array(value_list)
 unc_array=SPRT.lists2unc_array(value_list,unc_list)
+
+print('value_list=',value_list)
+print('unc_list=',unc_list)
 print(unc_array)
 
 
