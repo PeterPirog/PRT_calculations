8904e1aaf838d8c28507c0cfe4804021e15206d5
peterpirogtf <peterpirogtf@gmail.com>
2020-04-01 15:04:36 +0200

    New structure of modules. Separated ITS and GTCext functions

--- calculate_SPRT_01.py
+++ calculate_SPRT_01.py
@@ -3,18 +3,18 @@ import GTC
 import GTCext
 import ITS90
 
-#Funkcja rezystancji zredukowanej W(t) ze świadectwa wzorcowania
-Wt=[4.28556295,3.37543469,2.56855103,1.89259628]
-#zamiana funkcji na tablicę z niepewnościami
+# Funkcja rezystancji zredukowanej W(t) ze świadectwa wzorcowania
+Wt = [4.28556295, 3.37543469, 2.56855103, 1.89259628]
+# zamiana funkcji na tablicę z niepewnościami
 
-Wt=GTCext.urealext(value_list=Wt, unc_list=0.0, k=2, array_label='Wt', df=GTC.inf)
-print('W(t)=',Wt)
+Wt = GTCext.urealext(value_list=Wt, unc_list=0.0, k=2, array_label='Wt', df=GTC.inf)
+print('W(t)=', Wt)
 
-#Wprowadzenie temperatur ze świadectwa
-t=[961.78,660.323,419.527,231.928]
-ut=[0.0052,0.004,0.0017,0.0015]
-t=GTCext.urealext(value_list=t, unc_list=ut, k=2, array_label='t', df=GTC.inf)
-print('t=',t)
+# Wprowadzenie temperatur ze świadectwa
+t = [961.78, 660.323, 419.527, 231.928]
+ut = [0.0052, 0.004, 0.0017, 0.0015]
+t = GTCext.urealext(value_list=t, unc_list=ut, k=2, array_label='t', df=GTC.inf)
+print('t=', t)
 
-Wrt=ITS90.Calculate_Wr(GTC.ureal(1,0.01))
+Wrt = ITS90.Calculate_Wr(GTC.ureal(1, 0.01))
 print(Wrt)
