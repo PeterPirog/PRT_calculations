3ec620be7a489812e446cae2bba03331fbf6954c
Slawomir1900 <slawdabr@wp.pl>
2020-05-18 10:39:56 +0200

    update-tst

--- CvD_coeff_pinv.py
+++ CvD_coeff_pinv.py
@@ -38,6 +38,12 @@ R0=R[2]
 T = [-80.0078, -38.8397, 0, 231.9287, 419.5246, 660.323]
 UT=[0.027,0.025,0.025,0.029,0.048,0.022]
 
+
+
+
+
+
+
 R=urealext(value_list=R, unc_list=UR, k=2, array_label='R', df=GTC.inf)
 print("R=",R)
 T=urealext(value_list=T, unc_list=UT, k=2, array_label='T', df=GTC.inf)
@@ -81,4 +87,49 @@ print("\nB=",B.x)
 print("\nC=",C.x)
 print("\nUncertainty budget for A")
 for l,u in GTC.reporting.budget(A,reverse=True):
-    print("{0}:{1:G}".format(l,u))
+    print("{0}:  {1:G}".format(l,u))
+
+
+def CVD_R(t,R0, A, B, C):
+    C=C*(np.sign(t)-1)/(-2)
+    R = R0 * (1 + (A * t) + (B * t ** 2) + C * (t - 100) * t ** 3)
+    return R
+
+def dRdt(t,R0, A, B, C):
+    C=C*(np.sign(t)-1)/(-2)
+    dR_dt = R0 *((A+2*B*t)+C*(4*t**3-300*t**2))
+    return dR_dt
+def dtdR(t,R0, A, B, C):
+    dt_dR=1/dRdt(t,R0, A, B, C)
+    return dt_dR
+
+T_range=np.arange(-80,660,1)
+print(T_range)
+print("A=",A)
+"""
+A=ureal(3.984201E-3,A.u)
+B=ureal(-5.86858E-7,B.u)
+C=ureal(-6.24817E-12,C.u)
+"""
+
+k=1
+R_calc=[CVD_R(T_range[i],R0, A, B, C).x for i in range(len(T_range))]
+print("R_calc=",R_calc)
+uR_calc=[k*CVD_R(T_range[i],R0, A, B, C).u for i in range(len(T_range))]
+print("UR_calc=",uR_calc)
+
+C_dtdR=[dtdR(T_range[i],R0, A, B, C).x for i in range(len(T_range))]
+print("dtdR=",C_dtdR)
+
+uT=[C_dtdR[i]*uR_calc[i] for i in range(len(T_range))]
+print("uT",uT)
+
+import matplotlib.pyplot as plt
+
+plt.plot(T_range,uT)
+plt.ylabel('Standard uncertainty C')
+plt.xlabel('Temperature C')
+plt.show()
+
+#for l,u in GTC.reporting.budget(A,reverse=True):
+ #   print("{0}:  {1:G}".format(l,u))
