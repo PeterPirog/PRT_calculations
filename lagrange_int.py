e4842e06e89abe940318f78566e8545309c2e0a3
peterpirogtf <peterpirogtf@gmail.com>
2020-05-19 15:05:27 +0200

    example with range 7 ITS coeff calculation - added

--- lagrange_int.py
+++ lagrange_int.py
@@ -1,52 +1,43 @@
 import GTC
-
 import GTCext
-
-
-X=GTCext.urealext(value_list=[1,2,3,4,5,6], unc_list=0.1, k=2, array_label='X', df=GTC.inf)
-Y=GTCext.urealext(value_list=[2,3,7,4,2,1], unc_list=0.1, k=2, array_label='Y', df=GTC.inf)
-"""
-print('X=',X)
-print('Y=',Y)
-
-N=X.shape[1]
-
-"""
 from functools import reduce
+import numpy as np
+
 
 def lagrange_productsK(X,x,k):
     N = X.shape[1]
-    print('N=',N)
-    #lag_prod = [i for i in range(N)]
-    #lag_prod=[(x-X[0,i])/(X[0,k]-X[0,i]) for i in range(N) if k is not i else 1]
-    #[unicode(x.strip()) if x is not None else '' for x in row]
     lag_prod = [1 if k==i else (x - X[0, i]) / (X[0, k] - X[0, i]) for i in range(N)]
-    print('lag_prod=', lag_prod)
-    #prod=[lag_prod[0] if i==0 else prod[i-1]*prod[i] for i in range(N)]
     prod= reduce((lambda x, y: x * y), lag_prod)
-    print('prod=',prod)
     return prod
-"""
-prod=lagrange_productsK(X,1.5,2)
-print(prod)
-x=5
-Px_list=[Y[0,k]*lagrange_productsK(X,x,k) for k in range(N)]
-print('Px_list=',Px_list)
 
-Px= reduce((lambda x, y: x + y), Px_list)
-print('P(x)=',Px,' for x=',x)
-"""
 def calculate_lagrange_val(X_array,Y_array,x):
     N = X_array.shape[1]
     Px_list = [Y_array[0, k] * lagrange_productsK(X_array, x, k) for k in range(N)]
     Px = reduce((lambda x, y: x + y), Px_list)
     return Px
 
+
+
+#TEST
+
 X=GTCext.urealext(value_list=[1,2,3,4,5,6], unc_list=0.1, k=2, array_label='X', df=GTC.inf)
-Y=GTCext.urealext(value_list=[2,3,7,4,2,1], unc_list=0.1, k=2, array_label='Y', df=GTC.inf)
+Y=GTCext.urealext(value_list=[2,3,3,4,2,1], unc_list=0.1, k=2, array_label='Y', df=GTC.inf)
+
+#Single value
 x=2
 Px=calculate_lagrange_val(X,Y,x)
 print('P(x)=',Px,' for x=',x)
+#Single vector
+x=np.arange(1,6,0.1)
+y=[calculate_lagrange_val(X,Y,x[i]) for i in range(len(x))]
+yx=[y[i].x for i in range(len(x))]
+print(x)
+print(y)
+print(y,yx)
 
+import matplotlib.pyplot as plt
+plt.plot(x,yx)
+plt.ylabel('some numbers')
+plt.show()
 
 
