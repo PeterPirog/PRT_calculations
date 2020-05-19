bcd332a3894c2f47ca07f100374836d03f005f01
peterpirogtf <peterpirogtf@gmail.com>
2020-05-19 14:43:38 +0200

    example with range 7 ITS coeff calculation - added

--- lagrange_int.py
+++ lagrange_int.py
@@ -5,13 +5,13 @@ import GTCext
 
 X=GTCext.urealext(value_list=[1,2,3,4,5,6], unc_list=0.1, k=2, array_label='X', df=GTC.inf)
 Y=GTCext.urealext(value_list=[2,3,7,4,2,1], unc_list=0.1, k=2, array_label='Y', df=GTC.inf)
-
+"""
 print('X=',X)
 print('Y=',Y)
 
 N=X.shape[1]
 
-
+"""
 from functools import reduce
 
 def lagrange_productsK(X,x,k):
@@ -26,12 +26,27 @@ def lagrange_productsK(X,x,k):
     prod= reduce((lambda x, y: x * y), lag_prod)
     print('prod=',prod)
     return prod
-
+"""
 prod=lagrange_productsK(X,1.5,2)
 print(prod)
-x=1
+x=5
 Px_list=[Y[0,k]*lagrange_productsK(X,x,k) for k in range(N)]
 print('Px_list=',Px_list)
 
 Px= reduce((lambda x, y: x + y), Px_list)
 print('P(x)=',Px,' for x=',x)
+"""
+def calculate_lagrange_val(X_array,Y_array,x):
+    N = X_array.shape[1]
+    Px_list = [Y_array[0, k] * lagrange_productsK(X_array, x, k) for k in range(N)]
+    Px = reduce((lambda x, y: x + y), Px_list)
+    return Px
+
+X=GTCext.urealext(value_list=[1,2,3,4,5,6], unc_list=0.1, k=2, array_label='X', df=GTC.inf)
+Y=GTCext.urealext(value_list=[2,3,7,4,2,1], unc_list=0.1, k=2, array_label='Y', df=GTC.inf)
+x=2
+Px=calculate_lagrange_val(X,Y,x)
+print('P(x)=',Px,' for x=',x)
+
+
+
