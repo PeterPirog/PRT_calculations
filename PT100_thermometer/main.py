7e1e72173700fbfd624a67c7b6984256de878f42
peterpirogtf <peterpirogtf@gmail.com>
2021-04-14 14:55:40 +0200

    example with range 7 ITS coeff calculation - added

--- PT100_thermometer/main.py
+++ PT100_thermometer/main.py
@@ -6,7 +6,13 @@ class HP34401():
     def __init__(self, address):
         self.address = address
         self.resource_manager = pyvisa.ResourceManager()
-        self.current_res = None
+        self.current_res = None  # current measured resistance
+        self.range = None  # number of multimeter  range for measured resistance, first range=0
+
+        # data from service manual to calculate resistance uncertainty
+        self.R_ranges = np.array([100.0, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9])  # ranges
+        self.pct_of_read = np.array([0.01, 0.01, 0.01, 0.01, 0.012, 0.04, 0.8, 8])
+        self.pct_of_range = np.array([0.004, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001])
 
         try:
             self.inst = self.resource_manager.open_resource(self.address)
@@ -32,21 +38,17 @@ class HP34401():
         self.current_res = float(self.inst.query('MEASure:FRESistance?'))
         return self.current_res
 
-    def std_unc_for_R(self,R):
-        self.R_ranges=np.array([100.0,1e3,1e4,1e5,1e6,1e7,1e8,1e9])
-        self.pct_of_read = np.array([0.01,0.01,0.01,0.01,0.012,0.04,0.8,8])
-        self.pct_of_range = np.array([0.004,0.001,0.001,0.001,0.001,0.001,0.001,0.001])
-
-        #self.idx=np.where(self.R_ranges<R,0,1)
-        #print (idx)
-        if R <=100:
-            self.range=0
+    def error_for_R(self, R):
+        if R <= 100:
+            self.range = 0
         else:
-            self.range=np.argmax(self.R_ranges >R)
-        #print(self.range)
+            self.range = np.argmax(self.R_ranges > R)
 
-        return R*0.01*self.pct_of_read[self.range]+0.01*self.pct_of_range[self.range]*self.R_ranges[self.range]
+        return R * 0.01 * self.pct_of_read[self.range] + 0.01 * self.pct_of_range[self.range] * self.R_ranges[
+            self.range]
 
+    def std_unc_for_R(self, R):
+        return self.error_for_R(R) / np.sqrt(3)
 
 
 class PT100():
@@ -60,16 +62,26 @@ class PT100():
         self.coef_C = coef_C
 
         self.current_Rt = None
-        self.current_t = None
+        self.current_Rt_std_unc = None  # standard uncertainty of resistance
+        self.current_Rt_ext_unc = None  # extended uncertainty of resistance
 
-    def meas_temp(self,verbose=True):
+        self.current_t = None  # calculated temperature
+        self.current_t_std_unc = None  # standard uncertainty of temperature
+        self.current_t_ext_unc = None  # extended uncertainty of temperature
+
+        self.k = 2  # expanded uncertainty factor
+
+    def meas_temp(self, verbose=True):
         self.current_Rt = self.multimeter.measure_4R()
         self.current_Rt_std_unc = self.multimeter.std_unc_for_R(self.current_Rt)  #########0.13
-
+        self.current_Rt_ext_unc = self.k * self.current_Rt_std_unc
 
         self.current_t, self.current_t_std_unc = self.convert_R_to_t(self.current_Rt, self.current_Rt_std_unc)
+        self.current_t_ext_unc = self.k * self.current_t_std_unc
+
         if verbose:
-            print(f'Resistance {self.current_Rt}+/-{self.current_Rt_std_unc} ohm, temperature:{self.current_t} C, std unc:{self.current_t_std_unc} C')
+            print(f'Resistance {self.current_Rt}+/-{self.current_Rt_std_unc} ohm,'
+                  f' temperature:{self.current_t} C, std unc:{self.current_t_std_unc} C')
 
         return self.current_t, self.current_t_std_unc
 
@@ -96,8 +108,8 @@ class PT100():
         return t
 
     def convert_R_to_t(self, R_value, R_std_unc=0.0, N=1000):
-        Rx_list = []  # list of resistances generated for assumed standard uncertainty
-        tx_list = []  # list of temperatures generated for assumed standard uncertainty
+        # Rx_list - list of resistances generated for assumed standard uncertainty
+        # tx_list - list of temperatures generated for assumed standard uncertainty
 
         if R_std_unc == 0.0:
             return self.__R2t__(R_value), 0.0
@@ -113,5 +125,5 @@ if __name__ == "__main__":
 
     print(pt.meas_temp())
     # print(pt.convert_R_to_t(120,0.1))
-    #mul=HP34401(address='GPIB0::21::INSTR')
-    #print(mul.std_unc_for_R(313.7))
+    # mul=HP34401(address='GPIB0::21::INSTR')
+    # print(mul.std_unc_for_R(313.7))
